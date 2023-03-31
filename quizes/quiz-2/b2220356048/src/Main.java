06db5240c181165309fcd2b8a89e4d605b2561f2
Yusufpek <yusuf55ipek55@gmail.com>
2023-04-01 01:27:44 +0300

    updated code for warnings

--- quizes/quiz-2/b2220356048/src/Main.java
+++ quizes/quiz-2/b2220356048/src/Main.java
@@ -3,7 +3,7 @@ class Main {
     public static void main(String[] args) {
         IO io = new IO("input.txt", "output.txt");
         TripSchedule trips = new TripSchedule(InputParser.parse(io.readInputFile()));
-        TripController tripController = new TripController(io, trips);
+        new TripController(io, trips);
         io.writeToFile();
     }
 
--- quizes/quiz-2/b2220356048/src/TripSchedule.java
+++ quizes/quiz-2/b2220356048/src/TripSchedule.java
@@ -1,10 +1,7 @@
-import java.util.List;
-
 public class TripSchedule {
     public Trip[] trips;
 
     TripSchedule(Trip[] tripList) {
         this.trips = tripList;
-
     }
 }
