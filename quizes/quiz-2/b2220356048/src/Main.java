ebafa0f410108e5b92303575e2b4155fd8ac44e5
Yusufpek <yusuf55ipek55@gmail.com>
2023-04-01 01:43:38 +0300

    args added, gitignore updated

--- quizes/quiz-2/b2220356048/src/Main.java
+++ quizes/quiz-2/b2220356048/src/Main.java
@@ -1,7 +1,7 @@
 
 class Main {
     public static void main(String[] args) {
-        IO io = new IO("input.txt", "output.txt");
+        IO io = new IO(args[0], args[1]);
         TripSchedule trips = new TripSchedule(InputParser.parse(io.readInputFile()));
         new TripController(io, trips);
         io.writeToFile();
