36afb840f09947664dcb102bcd0afefde8279656
Yusufpek <yusuf55ipek55@gmail.com>
2023-04-01 18:24:20 +0300

    bug fix (delayed state for departure time added)

--- quizes/quiz-2/b2220356048/src/TripController.java
+++ quizes/quiz-2/b2220356048/src/TripController.java
@@ -3,12 +3,14 @@ import java.util.Comparator;
 
 public class TripController implements DepartureController, ArrivalController {
     IO io;
-    TripSchedule tripSchedule;
+    protected TripSchedule tripSchedule;
 
     TripController(IO io, TripSchedule tripSchedule) {
         this.io = io;
+        this.tripSchedule = tripSchedule;
         DepartureSchedule(tripSchedule);
         io.outputStrings.add(""); // new line
+        setAllTripsState(STATE_IDLE); // set all states to idle again
         ArrivalSchedule(tripSchedule);
     }
 
@@ -31,8 +33,19 @@ public class TripController implements DepartureController, ArrivalController {
     public void DepartureSchedule(TripSchedule tripSchedule) {
         io.outputStrings.add("Departure order:");
         Arrays.sort(tripSchedule.trips, Comparator.comparing(Trip::getDepartureTime));
+        for (int i = 0; i < (tripSchedule.trips.length - 1); i++) {
+            if (tripSchedule.trips[i].departureTime.equals(tripSchedule.trips[i + 1].departureTime)) {
+                tripSchedule.trips[i].setState(STATE_DELAYED);
+                tripSchedule.trips[i + 1].setState(STATE_DELAYED);
+            }
+            io.outputStrings.add(tripSchedule.trips[i].departureString());
+        }
+        io.outputStrings.add(tripSchedule.trips[tripSchedule.trips.length - 1].departureString()); // print last item
+    }
+
+    private void setAllTripsState(String state) {
         for (Trip trip : tripSchedule.trips) {
-            io.outputStrings.add(trip.departureString());
+            trip.setState(state);
         }
     }
 
