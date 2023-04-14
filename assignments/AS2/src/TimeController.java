a77ebf5108fb4ed7500f3bef891eab7afe14c817
Yusufpek <yusuf55ipek55@gmail.com>
2023-04-15 02:24:16 +0300

    updated now (made static), added new validations

--- assignments/AS2/src/TimeController.java
+++ assignments/AS2/src/TimeController.java
@@ -3,7 +3,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeController {
-    Date now;
+    static Date now; // make static for reaching everywhere
 
     /**
      * @param time
@@ -13,22 +13,23 @@ public class TimeController {
             if (!dateFormat.format(dateFormat.parse(time)).equals(time)) {
                 throw new ParseException("wrong date", 0); // throw error
             }
-            this.now = dateFormat.parse(time);
+            TimeController.now = dateFormat.parse(time);
             IO.outputStrings.add(String.format("SUCCESS: Time has been set to %s!", time));
         } catch (ParseException e) {
-            IO.outputStrings.add("ERROR: Time format is not correct!");
+            TimeController.now = null;
+            IO.outputStrings.add("ERROR: Format of the initial date is wrong! Program is going to terminate!");
         }
     }
 
     boolean setTime(String time) {
         try {
             Date newDate = dateFormat.parse(time);
-            if (newDate.getTime() - this.now.getTime() < 0) {
-                IO.outputStrings.add("ERROR: Time cannot be reversed!");
+            if (newDate.getTime() - TimeController.now.getTime() < 0) {
+                IO.outputStrings.add(REVERSED_TIME_ERROR);
             } else if (!dateFormat.format(dateFormat.parse(time)).equals(time)) {
                 throw new ParseException("wrong date", 0); // throw error
             } else {
-                this.now = newDate;
+                TimeController.now = newDate;
                 return true;
             }
         } catch (ParseException e) {
@@ -38,16 +39,25 @@ public class TimeController {
     }
 
     void setTime(Date time) {
-        this.now = time;
+        TimeController.now = time;
     }
 
     void skipMinutes(int minute) {
-        this.now = new Date(now.getTime() + minute * 60 * 1000);
+        if (minute < 0) {
+            IO.outputStrings.add(REVERSED_TIME_ERROR);
+            return;
+        }
+        if (minute == 0) {
+            IO.outputStrings.add("ERROR: There is nothing to skip!");
+            return;
+        }
+        TimeController.now = new Date(now.getTime() + minute * 60 * 1000);
     }
 
     String getTime() {
-        return dateFormat.format(now);
+        return dateFormat.format(TimeController.now);
     }
 
+    private final String REVERSED_TIME_ERROR = "ERROR: Time cannot be reversed!";
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
 }
