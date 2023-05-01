d46f9d2dc651e6208afe3f466529f4bfe427fc7d
Yusufpek <yusuf55ipek55@gmail.com>
2023-05-01 11:49:05 +0300

    as3 files uploaded

---
+++ assignments/AS3/src/IO.java
@@ -0,0 +1,68 @@
+import java.io.IOException; //Exception Handling
+import java.nio.file.Files; //Reading and writing
+import java.nio.file.Paths; //Reaching io files
+import java.util.ArrayList; //Output Strings
+import java.util.List;
+import java.io.FileWriter; // write to output file
+
+public class IO {
+    private String inputFileName;
+    private String outputFileName;
+    public static ArrayList<String> outputStrings; // output strings array for outputs line
+
+    IO(String inputFileName, String outputFileName) {
+        this.inputFileName = inputFileName;
+        this.outputFileName = outputFileName;
+        IO.outputStrings = new ArrayList<String>();
+    }
+
+    List<String> readInputFile(boolean discardEmptyLines) {
+        try {
+            List<String> inputs = Files.readAllLines(Paths.get(inputFileName));
+            if (discardEmptyLines) {
+                int i = 0;
+                while (i < inputs.size()) {
+                    if (inputs.get(i).trim().equals("")) // get the line and trim it if it is empty
+                        inputs.remove(i); // delete the line
+                    else
+                        i++;
+
+                }
+            }
+            return inputs;
+        } catch (IOException exception) {
+            System.out.println(exception);
+            return null;
+        }
+    }
+
+    boolean writeToFile() {
+        try {
+            FileWriter myWriter = new FileWriter(outputFileName);
+            for (String line : outputStrings) {
+                myWriter.write(line + "\n");
+            }
+            myWriter.close();
+            return true;
+        } catch (IOException exception) {
+            System.out.println(exception);
+            return false;
+        }
+    }
+
+    String getInputFileName() {
+        return this.inputFileName;
+    }
+
+    String getOutputFileName() {
+        return this.outputFileName;
+    }
+
+    void setInputFileName(String inputFileName) {
+        this.inputFileName = inputFileName;
+    }
+
+    void setOutputFileName(String outputFileName) {
+        this.outputFileName = outputFileName;
+    }
+}
---
+++ assignments/AS3/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class Main {
+    public static void main(String[] args) {
+        IO io = new IO(args[0], args[1]); // set the file names with command line arguments.
+        List<String> commands = io.readInputFile(true);
+        io.writeToFile();
+    }
+}
