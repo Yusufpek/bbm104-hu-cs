b243aa0864990ea20f596641f6fb3c26e6cee70f
Yusufpek <yusuf55ipek55@gmail.com>
2023-05-08 10:22:52 +0300

    base model added

---
+++ assignments/AS3/src/LibraryObject.java
@@ -0,0 +1,15 @@
+public class LibraryObject {
+    private int id;
+
+    public LibraryObject(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
