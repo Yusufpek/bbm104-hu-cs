122c9826461d82301ab135f3e639215f36754298
Yusufpek <yusuf55ipek55@gmail.com>
2023-05-19 03:27:19 +0300

    constant images added

---
+++ assignments/AS4/src/Images.java
@@ -0,0 +1,20 @@
+public class Images {
+
+    static final String TYPE = ".png";
+    static final String BASE = "../assets/";
+    static final String WELCOME = BASE + "welcome/"; // 1.png
+    static final String BACKGROUND = BASE + "background/"; // 1-6.png
+    static final String FOREGROUND = BASE + "foreground/"; // 1-6.png
+    static final String CROSSHAIR = BASE + "crosshair/"; // 1-7.png
+    static final String FAVICON = BASE + "favicon/"; // 1.png
+
+    static final String DUCK = BASE + "duck_"; // 1-8.png
+    static final String DUCK_BLACK = DUCK + "black/"; // 1-8.png
+
+    static String getName(String name, String path) {
+        if (path != "") {
+            return path + name + TYPE;
+        }
+        return name + TYPE;
+    }
+}
