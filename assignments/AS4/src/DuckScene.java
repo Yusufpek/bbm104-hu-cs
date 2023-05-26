6332543b2b98c70f248df63c762ba3e4b9159ac9
Yusufpek <yusuf55ipek55@gmail.com>
2023-05-26 16:06:19 +0300

    restart game added

--- assignments/AS4/src/DuckScene.java
+++ assignments/AS4/src/DuckScene.java
@@ -2,13 +2,15 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 
 public class DuckScene extends Scene {
-    final private static WelcomePane welcomePane = new WelcomePane();
-    final private static GamePane gamePane = new GamePane();
+    private static WelcomePane welcomePane = new WelcomePane();
+    private static GamePane gamePane = new GamePane();
     static boolean isSetting = false;
     static boolean isUnlimitedAmmo = false;
+    private boolean isFirst = true;
 
     DuckScene() {
         super(welcomePane, ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT);
+        gamePane = new GamePane(this);
         this.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
             switch (key.getCode()) {
                 case ENTER:
@@ -18,44 +20,38 @@ public class DuckScene extends Scene {
                         System.out.println("navigate");
                     } else {
                         if (this.getRoot() != gamePane) {
-                            int crossId = Math.abs(welcomePane.getCurrentCross() % welcomePane.crosshairs.length) + 1;
-                            int backgroundId = Math
-                                    .abs(welcomePane.getCurrentBackgroundIndex() % welcomePane.backgrounds.length) + 1;
-                            gamePane.setCrossId(crossId);
-                            gamePane.setBackgroundId(backgroundId);
-                            gamePane.setFocusTraversable(true);
-                            this.setRoot(gamePane);
-                            gamePane.requestFocus();
+                            navigateGame();
                         }
                     }
                     break;
                 case ESCAPE:
                     System.out.println("Escape");
-                    if (this.getRoot() == welcomePane)
+                    if (this.getRoot() == welcomePane && isFirst)
                         System.exit(0);
+                    isFirst = true;
                     break;
                 case RIGHT:
                     System.out.println("Right");
                     if (isSetting) {
-                        welcomePane.currentBackgroundIndex++;
+                        WelcomePane.currentBackgroundIndex++;
                         welcomePane.updateScreen();
                     }
                     break;
                 case LEFT:
                     if (isSetting) {
-                        welcomePane.currentBackgroundIndex--;
+                        WelcomePane.currentBackgroundIndex--;
                         welcomePane.updateScreen();
                     }
                     break;
                 case UP:
                     if (isSetting) {
-                        welcomePane.currentCross++;
+                        WelcomePane.currentCross++;
                         welcomePane.updateScreen();
                     }
                     break;
                 case DOWN:
                     if (isSetting) {
-                        welcomePane.currentCross--;
+                        WelcomePane.currentCross--;
                         welcomePane.updateScreen();
                     }
                     break;
@@ -71,4 +67,33 @@ public class DuckScene extends Scene {
         });
     }
 
+    void navigateGame() {
+        gamePane = new GamePane(this);
+        int crossId = Math.abs(welcomePane.getCurrentCross() % welcomePane.crosshairs.length) + 1;
+        int backgroundId = Math
+                .abs(welcomePane.getCurrentBackgroundIndex() % welcomePane.backgrounds.length) + 1;
+        System.out.println(crossId + " " + backgroundId);
+        gamePane.setCrossId(crossId);
+        gamePane.setBackgroundId(backgroundId);
+        gamePane.setFocusTraversable(true);
+        this.setRoot(gamePane);
+        gamePane.requestFocus();
+    }
+
+    void setCurrentPane(Panes pane) {
+        isFirst = false;
+        gamePane = new GamePane(this);
+        welcomePane = new WelcomePane();
+        isSetting = false;
+
+        if (pane == Panes.GAME)
+            navigateGame();
+        else
+            this.setRoot(welcomePane);
+    }
+
+    enum Panes {
+        WELCOME,
+        GAME
+    }
 }
