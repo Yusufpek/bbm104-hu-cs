4f4fd653fa15e1c6b5ee08ccfe42beabfc8e6190
Yusufpek <yusuf55ipek55@gmail.com>
2023-05-29 12:45:06 +0300

    end of the level texts added, navigations set

--- assignments/AS4/src/GamePane.java
+++ assignments/AS4/src/GamePane.java
@@ -2,17 +2,24 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 import javafx.scene.ImageCursor;
+import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
-public class GamePane extends Pane implements Function {
+public class GamePane extends Pane {
     int crossId;
     int backgroundId;
     Level level;
+    DuckScene scene;
 
     GamePane() {
         super();
         level = new Level();
+    }
+
+    GamePane(DuckScene scene) {
+        this();
+        this.scene = scene;
     }
 
     GamePane(int cross, int background) {
@@ -38,14 +45,13 @@ public class GamePane extends Pane implements Function {
     }
 
     public void setBackgroundId(int backgroundId) {
+        removeWidget(Texts.nextRoundTextFlow, Texts.noAmmoTextFlow);
         this.backgroundId = backgroundId;
         try {
             CustomText levelText = new CustomText("Level: " + level.level + "/" + Level.MAX_LEVEL, 8, 0.05); // top of
             CustomText ammoText = new CustomText("Ammo Left: " + level.ammo, 8, 0.05); // top of the screen
             ammoText.setTranslateX(ScreenSize.getWidth(0.8)); // right of the screen
-            System.out.println("Width: " + ScreenSize.SCREEN_WIDTH);
-            CustomText duckText = new CustomText("Duck Left: " + (level.duckCount - level.killedDuckCount), 8, 0.06);
-            duckText.setTranslateX(0);
+            // set the ducks
             Duck[] ducks = new Duck[level.duckCount];
             for (int i = 0; i < level.duckCount; i++) {
                 ducks[i] = new Duck(level.duckNames[i], level.duckVerticalMovement[i], level);
@@ -55,46 +61,64 @@ public class GamePane extends Pane implements Function {
             this.getChildren().add(new CustomImageView(backgroundId + "", Images.BACKGROUND));
             this.getChildren().addAll(ducks);
             this.getChildren().add(new CustomImageView(backgroundId + "", Images.FOREGROUND));
-            this.getChildren().addAll(levelText, ammoText, duckText);
+            this.getChildren().addAll(levelText, ammoText);
 
+            // Key Control
             this.setOnKeyPressed(key -> {
                 System.out.println("Level: " + level.level);
                 System.out.println(key.getCode());
-                System.out.println(
-                        key.getCode() == KeyCode.ENTER && this.getChildren().contains(Texts.roundFinishedText));
-                if (key.getCode() == KeyCode.ENTER && this.getChildren().contains(Texts.roundFinishedText)) {
+                System.out.println("is finished: " + level.isFinished());
+                if (key.getCode() == KeyCode.ENTER && this.getChildren().contains(Texts.nextRoundTextFlow)) {
                     System.out.println("round finish enter");
-                    this.getChildren().remove(Texts.roundFinishedText);
+                    this.getChildren().remove(Texts.winText);
+                    level.incrementLevelCount();
                     setBackgroundId(this.backgroundId);
+                } else if (this.getChildren().contains(Texts.noAmmoTextFlow)
+                        || this.getChildren().contains(Texts.gameCompletedTextFlow)) {
+                    if (key.getCode() == KeyCode.ENTER)
+                        scene.setCurrentPane(DuckScene.Panes.GAME);
+                    else if (key.getCode() == KeyCode.ESCAPE) {
+                        System.out.println("go to welcome pane");
+                        scene.setCurrentPane(DuckScene.Panes.WELCOME);
+                    }
                 }
             });
 
             this.setOnMouseClicked((event) -> {
-                System.out.println(level.ammo);
-                if (level.ammo > 0 && !MainApplication.isUnlimitedAmmo) {
+                System.out.println("ammo: " + level.ammo);
+                // duck kill check
+                if (level.ammo > 0) {
+                    for (Duck duck : ducks) {
+                        if (duck.getBoundsInParent().contains(event.getX(), event.getY()) && !duck.isKilled) {
+                            level.killedDuckCount++;
+                            duck.stop();
+                            duck.setDeathImage();
+                            duck.deathAnimation();
+                            if (level.level < Level.MAX_LEVEL && level.isFinished()) {
+                                this.getChildren().add(Texts.nextRoundTextFlow);
+                            }
+                            System.out.println("duck dead");
+                        }
+                    }
+                    // max level completed check
+                    if (level.level == Level.MAX_LEVEL && level.isFinished()) {
+                        this.getChildren().add(Texts.gameCompletedTextFlow);
+                        System.out.println("game over, all levels finished");
+                    }
+                }
+                // increase ammo
+                if (level.ammo > 0 && !DuckScene.isUnlimitedAmmo) {
                     level.ammo -= 1;
                     ammoText.setText("Ammo Left: " + level.ammo);
                 }
-
-                double mouseX = event.getX();
-                double mouseY = event.getY();
-
-                // Iterate through the children (ducks) in reverse order to check the topmost
-                // duck first
-                for (Duck duck : ducks) {
-                    if (duck.getBoundsInParent().contains(mouseX, mouseY) && !duck.isKilled) {
-                        level.killedDuckCount++;
-                        duck.stop();
-                        duck.setDeathImage();
-                        duck.deathAnimation(this);
-                        // after finishing animation
-                        System.out.println("duck dead");
+                // ammo check
+                if (level.ammo <= 0 && !level.isFinished()) {
+                    if (!this.getChildren().contains(Texts.noAmmoTextFlow)) {
+                        if (!Texts.noAmmoTextFlow.getChildren().contains(Texts.gameOverEnterText))
+                            Texts.noAmmoTextFlow.getChildren().addAll(Texts.gameOverEnterText,
+                                    Texts.gameOverEscapeText);
+                        this.getChildren().add(Texts.noAmmoTextFlow);
                     }
-                }
-
-                if (level.ammo <= 0 && level.duckCount != level.killedDuckCount) {
-                    if (!this.getChildren().contains(Texts.noAmmoText))
-                        this.getChildren().add(Texts.noAmmoText);
                     System.out.println(ScreenSize.SCREEN_HEIGHT);
                     Arrays.stream(ducks).forEach(duck -> duck.stop());
                 }
@@ -104,18 +128,10 @@ public class GamePane extends Pane implements Function {
         }
     }
 
-    @Override
-    public void onDuckKilled() {
-        // this.getChildren().remove(duck);
-        if (level.killedDuckCount == level.duckCount) {
-            level.incrementLevelCount();
-            if (level.level <= Level.MAX_LEVEL)
-                this.getChildren().add(Texts.roundFinishedText);
-            else {
-                this.getChildren().add(Texts.gameOverText);
-                System.out.println("game over, all levels finished");
-            }
+    private void removeWidget(Node... widgets) {
+        for (Node node : widgets) {
+            if (this.getChildren().contains(node))
+                this.getChildren().remove(node);
         }
-        System.out.println("on duck killed function");
     }
 }
