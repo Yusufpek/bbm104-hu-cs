5128f41e10c5fc20f5bb08a764cab3ea2b94eb7f
Yusufpek <yusuf55ipek55@gmail.com>
2023-06-04 03:21:37 +0300

    main updated

--- quizes/quiz-5/b2220356048/src/Main.java
+++ quizes/quiz-5/b2220356048/src/Main.java
@@ -1,8 +1,8 @@
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         IO io = new IO(args[0], args[1]);
-        List<String> inputs = io.readInputFile();
+        Queue<String> inputs = io.readInputFile();
+        new InputParser(inputs);
+        io.writeToFile();
     }
 }
